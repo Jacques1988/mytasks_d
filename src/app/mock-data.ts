67c685b7f52b1179d99047d1be96ae8821bae2a1
Jacques <jacquesvanluyck@gmail.com>
2025-03-28 10:45:17 +0100

    update styling of task App

---
+++ src/app/mock-data.ts
@@ -0,0 +1,32 @@
+export let tasks  = [
+    {
+        id: 1,
+        task: "Blumen gießen",
+        status: "incomplete",
+        checked: false
+    },
+    {
+        id: 2,
+        task: "Esel knuddeln",
+        status: "completed",
+        checked: true
+    },
+    {
+        id: 3,
+        task: "Gänse füttern",
+        status: "incomplete",
+        checked: false
+    },
+    {
+        id: 4,
+        task: "Einkaufen",
+        status: "incomplete",
+        checked: false
+    },
+    {
+        id: 5,
+        task: "Programmieren",
+        status: "incomplete",
+        checked: false
+    },
+]
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TasksService } from '../services/tasks.service';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  tasks = inject(TasksService).getAllTasks()
+
 
 }
